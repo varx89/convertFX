793fab50dcc4cfb087542eea13176283e10ee5b7
Andrei Varcus <andrei.varcus@yahoo.com>
2023-06-26 02:19:07 +0300

    finished dom update with all addons

--- assets/scripts/exchange.js
+++ assets/scripts/exchange.js
@@ -1,116 +1,21 @@
-const apiCurrencies = {
-    AUD: 'Australian Dollar',
-    BGN: 'Bulgarian Lev',
-    BRL: 'Brazilian Real',
-    CAD: 'Canadian Dollar',
-    CHF: 'Swiss Franc',
-    CNY: 'Chinese Renminbi Yuan',
-    CZK: 'Czech Koruna',
-    DKK: 'Danish Krone',
-    EUR: 'Euro',
-    GBP: 'British Pound',
-    HKD: 'Hong Kong Dollar',
-    HUF: 'Hungarian Forint',
-    IDR: 'Indonesian Rupiah',
-    ILS: 'Israeli New Sheqel',
-    INR: 'Indian Rupee',
-    ISK: 'Icelandic Króna',
-    JPY: 'Japanese Yen',
-    KRW: 'South Korean Won',
-    MXN: 'Mexican Peso',
-    MYR: 'Malaysian Ringgit',
-    NOK: 'Norwegian Krone',
-    NZD: 'New Zealand Dollar',
-    PHP: 'Philippine Peso',
-    PLN: 'Polish Złoty',
-    RON: 'Romanian Leu',
-    SEK: 'Swedish Krona',
-    SGD: 'Singapore Dollar',
-    THB: 'Thai Baht',
-    TRY: 'Turkish Lira',
-    USD: 'United States Dollar',
-    ZAR: 'South African Rand',
+const getCurrencies = async () => {
+    const host = 'api.frankfurter.app';
+    const response = await fetch(`https://${host}/currencies`);
+    body = await response.json();
+    return body;
 };
-
-const apiEuro = {
-    amount: 1.0,
-    base: 'EUR',
-    date: '2023-06-22',
-    rates: {
-        AUD: 1.6194,
-        BGN: 1.9558,
-        BRL: 5.2331,
-        CAD: 1.4463,
-        CHF: 0.9825,
-        CNY: 7.8866,
-        CZK: 23.683,
-        DKK: 7.4486,
-        GBP: 0.86115,
-        HKD: 8.5987,
-        HUF: 369.43,
-        IDR: 16426,
-        ILS: 3.9847,
-        INR: 90.03,
-        ISK: 148.5,
-        JPY: 156.05,
-        KRW: 1424.62,
-        MXN: 18.8431,
-        MYR: 5.1091,
-        NOK: 11.599,
-        NZD: 1.7698,
-        PHP: 61.133,
-        PLN: 4.4253,
-        RON: 4.9631,
-        SEK: 11.7224,
-        SGD: 1.4741,
-        THB: 38.535,
-        TRY: 26.625,
-        USD: 1.0985,
-        ZAR: 20.258,
-    },
-};
-
-const apiAll = {
-    amount: 1.0,
-    base: 'EUR',
-    date: '2023-06-22',
-    rates: {
-        AUD: 1.6194,
-        BGN: 1.9558,
-        BRL: 5.2331,
-        CAD: 1.4463,
-        CHF: 0.9825,
-        CNY: 7.8866,
-        CZK: 23.683,
-        DKK: 7.4486,
-        GBP: 0.86115,
-        HKD: 8.5987,
-        HUF: 369.43,
-        IDR: 16426,
-        ILS: 3.9847,
-        INR: 90.03,
-        ISK: 148.5,
-        JPY: 156.05,
-        KRW: 1424.62,
-        MXN: 18.8431,
-        MYR: 5.1091,
-        NOK: 11.599,
-        NZD: 1.7698,
-        PHP: 61.133,
-        PLN: 4.4253,
-        RON: 4.9631,
-        SEK: 11.7224,
-        SGD: 1.4741,
-        THB: 38.535,
-        TRY: 26.625,
-        USD: 1.0985,
-        ZAR: 20.258,
-    },
+const getCurrencyFrom = async (currency) => {
+    const host = 'api.frankfurter.app';
+    const response = await fetch(`https://${host}/latest?from=${currency}`);
+    const body = await response.json();
+    return body;
 };
 
 const baseEX = 'EUR';
-const baseCONV = 'USD';
-const getValueRate = (base, convx) => {
+const baseCONV = 'CAD';
+const getValueRate = async (base = baseEX, convx = baseCONV) => {
+    const apiAll = Object.assign({}, await getCurrencyFrom(base));
+
     if (base === apiAll.base) {
         return apiAll.rates[convx];
     } else {
@@ -119,39 +24,113 @@ const getValueRate = (base, convx) => {
 };
 
 const imgFlagURL = (currency) => {
-    const imgUrl = `https://currencyfreaks.com/photos/flags/${currency}.png`;
+    const imgUrl = `https://currencyfreaks.com/photos/flags/${currency.toLowerCase()}.png`;
     return imgUrl;
 };
+// calculate
 
-const showPairs = (first, second) => {
+const showPairs = async (first, second, value = 1000) => {
     first = first.toString();
     second = second.toString();
 
     const baseAmImg = document.getElementById('ex-image-flag');
-    baseAmImg.setAttribute('src', imgFlagURL(first.toLowerCase()));
+    baseAmImg.setAttribute('src', imgFlagURL(first));
     const baseExCurrency = document.getElementById('ex-currency');
     baseExCurrency.textContent = first;
 
     const baseConvImg = document.getElementById('conv-image-flag');
-    baseConvImg.setAttribute('src', imgFlagURL(second.toLowerCase()));
+    baseConvImg.setAttribute('src', imgFlagURL(second));
     const baseConvCurrency = document.getElementById('conv-currency');
     baseConvCurrency.textContent = second;
 
     const currentRateValue = document.getElementById('current-rate-value');
-    currentRateValue.textContent = `1 = ${getValueRate(first, second)}`;
+    currentRateValue.textContent = `1 = ${await getValueRate(first, second)}`;
+
+    const exInput = document.getElementById('ex-amount');
+    exInput.value = value;
+    const convInput = document.getElementById('conv-amount');
+    convInput.value = (value * (await getValueRate(first, second))).toFixed(2);
 };
 showPairs(baseEX, baseCONV);
 
-const dropExList = () => {
-    const li = document.createElement('li');
-    li.setAttribute('class', 'currency-item-am');
-    li.setAttribute('onclick', `showCurr('${baseEX}')`);
+const renderShowPairs = () => {
+    const baseExCurrency = document.getElementById('ex-currency');
+    const baseConvCurrency = document.getElementById('conv-currency');
+
+    showPairs(
+        baseExCurrency.textContent,
+        baseConvCurrency.textContent,
+        document.getElementById('ex-amount').value
+    );
+};
+
+document.getElementById('ex-amount').addEventListener('keyup', () => {
+    const baseExCurrency = document.getElementById('ex-currency');
+    const baseConvCurrency = document.getElementById('conv-currency');
+    showPairs(
+        baseExCurrency.textContent,
+        baseConvCurrency.textContent,
+        document.getElementById('ex-amount').value
+    );
+});
+
+const dropExList = async () => {
+    const listEX = document.getElementsByClassName('list-amount')[0];
+    let apiObj = Object.assign({}, await getCurrencies());
+
+    for (obj in apiObj) {
+        const li = document.createElement('li');
+        li.setAttribute('class', 'currency-item-am');
+        li.setAttribute('onclick', `showCurr('${obj}')`);
+        const img = document.createElement('img');
+        img.setAttribute('class', 'list-img');
+        img.setAttribute('src', imgFlagURL(obj));
+        const spanCurr = document.createElement('span');
+        spanCurr.setAttribute('class', 'list-currency');
+        spanCurr.textContent = ` ${obj}`;
+        const spanDot = document.createElement('span');
+        spanDot.textContent = '-';
+        const spanCurrName = document.createElement('span');
+        // spanCurrName.setAttribute('class', 'ex-name');
+        spanCurrName.textContent = ` ${apiObj[obj]}`;
+
+        li.append(img, spanCurr, spanDot, spanCurrName);
+        listEX.appendChild(li);
+    }
+};
+const dropConvList = async () => {
+    const listConv = document.getElementsByClassName('list-convert')[0];
+    const listWithout = document.getElementById('ex-currency');
+
+    let apiObj = Object.assign({}, await getCurrencies());
+    delete apiObj[listWithout.textContent];
+
+    for (obj in apiObj) {
+        const li = document.createElement('li');
+        li.setAttribute('class', 'currency-item-conv');
+        li.setAttribute('onclick', `showConvCurr('${obj}')`);
+        const img = document.createElement('img');
+        img.setAttribute('class', 'list-img');
+        img.setAttribute('src', imgFlagURL(obj));
+        const spanCurr = document.createElement('span');
+        spanCurr.setAttribute('class', 'list-currency');
+        spanCurr.textContent = ` ${obj}`;
+        const spanDot = document.createElement('span');
+        spanDot.textContent = '-';
+        const spanCurrName = document.createElement('span');
+        // spanCurrName.setAttribute('class', 'ex-name');
+        spanCurrName.textContent = ` ${apiObj[obj]}`;
+
+        li.append(img, spanCurr, spanDot, spanCurrName);
+        listConv.appendChild(li);
+    }
 };
 
 const dropdownAmount = () => {
     const listAmount = document.getElementsByClassName('list-amount')[0];
     const listConvert = document.getElementsByClassName('list-convert')[0];
     listAmount.classList.remove('list-amount-show-none');
+    dropExList();
     listAmount.classList.toggle('list-amount-show');
     listConvert.classList.add('list-convert-show-none');
 };
@@ -160,6 +139,7 @@ const dropdownConvert = () => {
     const listAmount = document.getElementsByClassName('list-amount')[0];
     const listConvert = document.getElementsByClassName('list-convert')[0];
     listConvert.classList.remove('list-convert-show-none');
+    dropConvList();
     listConvert.classList.toggle('list-convert-show');
     listAmount.classList.add('list-amount-show-none');
 };
@@ -179,6 +159,7 @@ const showCurr = (param) => {
             listAmount.classList.toggle('list-amount-show-none');
         }
     }
+    renderShowPairs();
 };
 
 const showConvCurr = (param) => {
@@ -197,4 +178,5 @@ const showConvCurr = (param) => {
             listConvert.classList.toggle('list-convert-show-none');
         }
     }
+    renderShowPairs();
 };
