be46efb156695b297b9ecfec9d19e7d54c30858c
Andrei Varcus <andrei.varcus@yahoo.com>
2023-06-25 21:10:03 +0300

    finished dom mandatory with function getValuerate() to collect data

--- assets/scripts/exchange.js
+++ assets/scripts/exchange.js
@@ -70,27 +70,83 @@ const apiEuro = {
     },
 };
 
+const apiAll = {
+    amount: 1.0,
+    base: 'EUR',
+    date: '2023-06-22',
+    rates: {
+        AUD: 1.6194,
+        BGN: 1.9558,
+        BRL: 5.2331,
+        CAD: 1.4463,
+        CHF: 0.9825,
+        CNY: 7.8866,
+        CZK: 23.683,
+        DKK: 7.4486,
+        GBP: 0.86115,
+        HKD: 8.5987,
+        HUF: 369.43,
+        IDR: 16426,
+        ILS: 3.9847,
+        INR: 90.03,
+        ISK: 148.5,
+        JPY: 156.05,
+        KRW: 1424.62,
+        MXN: 18.8431,
+        MYR: 5.1091,
+        NOK: 11.599,
+        NZD: 1.7698,
+        PHP: 61.133,
+        PLN: 4.4253,
+        RON: 4.9631,
+        SEK: 11.7224,
+        SGD: 1.4741,
+        THB: 38.535,
+        TRY: 26.625,
+        USD: 1.0985,
+        ZAR: 20.258,
+    },
+};
+
+const baseEX = 'EUR';
+const baseCONV = 'USD';
+const getValueRate = (base, convx) => {
+    if (base === apiAll.base) {
+        return apiAll.rates[convx];
+    } else {
+        return 0;
+    }
+};
+
+const imgFlagURL = (currency) => {
+    const imgUrl = `https://currencyfreaks.com/photos/flags/${currency}.png`;
+    return imgUrl;
+};
+
 const showPairs = (first, second) => {
     first = first.toString();
     second = second.toString();
 
     const baseAmImg = document.getElementById('ex-image-flag');
-    baseAmImg.setAttribute(
-        'src',
-        `https://currencyfreaks.com/photos/flags/${first.toLowerCase()}.png`
-    );
+    baseAmImg.setAttribute('src', imgFlagURL(first.toLowerCase()));
     const baseExCurrency = document.getElementById('ex-currency');
     baseExCurrency.textContent = first;
 
     const baseConvImg = document.getElementById('conv-image-flag');
-    baseConvImg.setAttribute(
-        'src',
-        `https://currencyfreaks.com/photos/flags/${second.toLowerCase()}.png`
-    );
+    baseConvImg.setAttribute('src', imgFlagURL(second.toLowerCase()));
     const baseConvCurrency = document.getElementById('conv-currency');
     baseConvCurrency.textContent = second;
+
+    const currentRateValue = document.getElementById('current-rate-value');
+    currentRateValue.textContent = `1 = ${getValueRate(first, second)}`;
 };
-showPairs(apiEuro.base, 'USD');
+showPairs(baseEX, baseCONV);
+
+const dropExList = () => {
+    const li = document.createElement('li');
+    li.setAttribute('class', 'currency-item-am');
+    li.setAttribute('onclick', `showCurr('${baseEX}')`);
+};
 
 const dropdownAmount = () => {
     const listAmount = document.getElementsByClassName('list-amount')[0];
